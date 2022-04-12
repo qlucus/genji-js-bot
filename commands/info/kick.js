a33445bebad2d36f794efe4a3e84ee9d81f9df78
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-12 20:50:06 +0300

    Update kick.js

--- commands/info/kick.js
+++ commands/info/kick.js
@@ -14,7 +14,6 @@ module.exports = {
         let member = message.mentions.members.first();
         if(!member) return message.reply("Please mention a valid member of this server");
         if(!member.kickable) return message.reply("I cannot kick this member!");
-        member.send("קיבלת קיק, חחח יא נוב");
         member.kick();
         message.channel.send(`${member} **has been Kicked.**`);
     },
