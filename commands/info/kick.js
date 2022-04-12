3160b6971bbaac399923065d5a80fdb3c66928dd
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-12 14:02:53 +0300

    Update kick.js

--- commands/info/kick.js
+++ commands/info/kick.js
@@ -10,10 +10,11 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, message, args) => {
+        message.delete();
         let member = message.mentions.members.first();
         if(!member) return message.reply("Please mention a valid member of this server");
         if(!member.kickable) return message.reply("I cannot kick this member!");
         member.kick();
-        message.channel.send(`${member} Kicked!`);
+        message.channel.send(`${member.tag} **has been Kicked.**`);
     },
 };
