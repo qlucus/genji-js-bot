a139eacd7e684004ca096684bd066fba9344285e
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-13 03:00:59 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,5 @@
 const client = require("../index");
 
 client.on("ready", () =>
-    client.user.setActivity("NetCloud", { type: "WATCHING" }),
     console.log(`${client.user.tag} is up and ready to go!`)
 );
