619dbc468771048781a55c81ae5521511793bfa2
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-13 13:48:28 +0300

    Update say.js

--- commands/info/say.js
+++ commands/info/say.js
@@ -10,24 +10,7 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, message, args) => {
-        message.delete();
-
-        if (!message.member.hasPermission("MANAGE_MESSAGES"))
-            return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
-
-        if (args.length < 0)
-            return message.reply("Nothing to say?").then(m => m.delete(5000));
-
-        const roleColor = message.guild.me.highestRole.hexColor;
-
-        if (args[0].toLowerCase() === "embed") {
-            const embed = new RichEmbed()
-                .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" : roleColor);
-
-            message.channel.send(embed);
-        } else {
-            message.channel.send(args.join(" "));
-        }
+        const SayMessage = message.content.slice(4).trim();
+        message.channel.send("**" + SayMessage + "**")
     },
 };
