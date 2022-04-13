abaa107d6e254975193b22ac7e389e3f1ad53a94
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-13 03:22:54 +0300

    Update map.js

--- commands/info/map.js
+++ commands/info/map.js
@@ -12,7 +12,7 @@ module.exports = {
     run: async (client, message, args) => {
         const embed = new MessageEmbed()
             .setColor("#00ceff")
-            .setImage({ url: "https://cdn.discordapp.com/attachments/955206144442384470/955568114916024371/unknown.png" })
+            .setImage("https://cdn.discordapp.com/attachments/955206144442384470/955568114916024371/unknown.png")
             .setAuthor({ name: "SlowCity | Map", iconURL: "https://media.discordapp.net/attachments/738901088526401537/957932363441131530/Screenshot_25.png" })
             .setDescription("זוהי המפה שנבנתה על ידי flayer\nהמפה עם זכויות יוצרים, כל העתקה תענש בחומרה!\nלעוד עדכונים כנסו לחדר עדכונים");
         message.channel.send({ embeds: [embed] });
