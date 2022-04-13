09955fe659e65b675d2c4ea6043bd3379dda8c9f
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-13 13:54:03 +0300

    Update index.js

--- handler/index.js
+++ handler/index.js
@@ -42,10 +42,10 @@ module.exports = async (client) => {
     });
     client.on("ready", async () => {
         // Register for a single guild
-        await client.guilds.cache.get("963236755828523018").commands.set(arrayOfSlashCommands);
+        //await client.guilds.cache.get("963236755828523018").commands.set(arrayOfSlashCommands);
 
         // Register for all the guilds the bot is in
-        // await client.application.commands.set(arrayOfSlashCommands);
+         await client.application.commands.set(arrayOfSlashCommands);
     });
 
     // mongoose
