99441369ec498a68b431a4a1bf852890178b35c8
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-13 05:28:15 +0300

    Create say.js

---
+++ commands/info/say.js
@@ -0,0 +1,33 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "say",
+    aliases: ['s'],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        message.delete();
+
+        if (!message.member.hasPermission("MANAGE_MESSAGES"))
+            return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
+
+        if (args.length < 0)
+            return message.reply("Nothing to say?").then(m => m.delete(5000));
+
+        const roleColor = message.guild.me.highestRole.hexColor;
+
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new RichEmbed()
+                .setDescription(args.slice(1).join(" "))
+                .setColor(roleColor === "#000000" ? "#ffffff" : roleColor);
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(args.join(" "));
+        }
+    },
+};
