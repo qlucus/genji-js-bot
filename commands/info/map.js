efa56a9b20b633872629dddef8f102ca4f09772b
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-13 03:36:15 +0300

    Update map.js

--- commands/info/map.js
+++ commands/info/map.js
@@ -13,8 +13,8 @@ module.exports = {
         const embed = new MessageEmbed()
             .setColor("#00ceff")
             .setTitle("SlowCity Map")
-            .setImage("https://cdn.discordapp.com/attachments/955206144442384470/955568114916024371/unknown.png")
-            .setAuthor({ name: "SlowCity | Map", iconURL: "https://media.discordapp.net/attachments/738901088526401537/957932363441131530/Screenshot_25.png" })
+            .setThumbnail("https://media.discordapp.net/attachments/738901088526401537/957932363441131530/Screenshot_25.png")
+            .setAuthor("SlowCity | Map")
             .setDescription("✦ **המפה נבנתה על ידי פלייר** ✦\n✦ **למפה יש זכויות יוצרים, כל עתקה זה עונש** ✦\n✦ **במפה תוכלו לראות את כל האזורים הנל** ✦\n✦ **אסור לנצל עבודות שיש במפה** ✦");
         message.channel.send({ embeds: [embed] });
     },
