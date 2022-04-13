6e3b584042b9bcf29964355a8cfb475cb74a3192
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-13 03:31:40 +0300

    Update map.js

--- commands/info/map.js
+++ commands/info/map.js
@@ -14,7 +14,7 @@ module.exports = {
             .setColor("#00ceff")
             .setImage("https://cdn.discordapp.com/attachments/955206144442384470/955568114916024371/unknown.png")
             .setAuthor({ name: "SlowCity | Map", iconURL: "https://media.discordapp.net/attachments/738901088526401537/957932363441131530/Screenshot_25.png" })
-            .setDescription("✦ **המפה נבנתה על ידי פלייר**\n✦ **למפה יש זכויות יוצרים, כל עתקה זה עונש**\n✦ **במפה תוכלו לראות את כל האזורים הנל**\n✦ **אסור לנצל עבודות שיש במפה**");
+            .setDescription("✦ **המפה נבנתה על ידי פלייר** ✦\n✦ **למפה יש זכויות יוצרים, כל עתקה זה עונש** ✦\n✦ **במפה תוכלו לראות את כל האזורים הנל** ✦\n✦ **אסור לנצל עבודות שיש במפה** ✦");
         message.channel.send({ embeds: [embed] });
     },
 };
