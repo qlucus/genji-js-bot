4b9e2801316a17d0c9b80ed9376fd338918fdbf8
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-13 14:14:58 +0300

    Create se.js

---
+++ commands/info/se.js
@@ -0,0 +1,19 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "sayembed",
+    aliases: ['se'],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        message.delete();
+        const SayMessage = message.content.slice(4).trim();
+        const embed = new MessageEmbed()
+            .setDescription(SayMessage)
+        message.channel.send({ embeds: [embed] });
+    },
+};
