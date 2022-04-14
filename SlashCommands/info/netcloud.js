302221594415d578e2a56448af5922c9daf252bb
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-15 02:06:53 +0300

    Update netcloud.js

--- SlashCommands/info/netcloud.js
+++ SlashCommands/info/netcloud.js
@@ -8,10 +8,10 @@ module.exports = {
             .setColor("#fff700")
             .setAuthor({ name: "FAQ: About NetCloud", iconURL: `https://media.discordapp.net/attachments/961883143411998752/964291071100198992/752SHABOTA.png?width=676&height=676` })
             .setFooter({ text: `NetCloud Company 🤖` })
-            .addField({ name: "Who are you?", value: "We are a company.", inline: false })
-            .addField({ name: "What the bot can do?", value: "undefind", inline: false })
-            .addField({ name: "When the company have started?", value: "April 11, 2022", inline: false })
-            .addField({ name: "What is your goal?", value: "Our goal is reaching 100 servers.", inline: false });
+            .addFields({ name: "Who are you?", value: "We are a company.", inline: false })
+            .addFields({ name: "What the bot can do?", value: "undefind", inline: false })
+            .addFields({ name: "When the company have started?", value: "April 11, 2022", inline: false })
+            .addFields({ name: "What is your goal?", value: "Our goal is reaching 100 servers.", inline: false });
 
         interaction.followUp({ embeds: [embed] })
     },
