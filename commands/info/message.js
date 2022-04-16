7c37f7176321b821a09cb1cad76fcf18caf03579
AlonKlein <68191656+MrSolover@users.noreply.github.com>
2022-04-16 21:08:48 +0300

    Update message.js

--- commands/info/message.js
+++ commands/info/message.js
@@ -11,9 +11,9 @@ module.exports = {
      */
     run: async (client, message, args) => {
         if (message.channelId === "964290303672614972") {
-            const anonymous = (' ').join(); 
+            const anonymous = message.content.slice(4).trim();
             message.delete();
-            message.channel.send(`${anonymous}`);
+            message.channel.send(anonymous);
         } else {
             message.channel.send("You have to type it in the channel `TEST`");
         }
